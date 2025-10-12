939ee9d06a4eab049a80f19c30e027657df8959e
Olli Hämäläinen <hamalainenolli@gmail.com>
2025-10-12 21:27:46 +0300

    Added templates folder and some html pages

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 from flask import Flask
+from flask import render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Heipparallaa!"
+    words = ["apina", "banaani", "cembalo"]
+    return render_template("index.html", message="Tervetuloa!", items=words)
+
+@app.route("/form")
+def form():
+    return render_template("form.html")
+
+@app.route("/result", methods=["POST"])
+def result():
+    message = request.form["message"]
+    return render_template("result.html", message=message)
