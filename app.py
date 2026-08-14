201296b280be1639098827dacf6cd6a3e3917edf
Olli Hämäläinen <hamalainenolli@gmail.com>
2026-08-14 15:36:26 +0300

    Added forum, you can send msg and view them and see the count of them

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
-from flask import Flask
-from flask import render_template, request
-import sqlite3
+from flask import Flask, redirect, render_template, request
+import db
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    db = sqlite3.connect("database.db")
     db.execute("INSERT INTO visits (visited_at) VALUES (datetime('now'))")
-    db.commit()
-    result = db.execute("SELECT COUNT(*) FROM visits").fetchone()
-    count = result[0]
-    db.close()
-    return "Sivua on ladattu " + str(count) + " kertaa"
+    visits_result = db.query("SELECT COUNT(*) FROM visits")
+    visit_count = visits_result[0][0]
+    
+    messages = db.query("SELECT content FROM messages")
+    message_count = len(messages)
+    
+    return render_template("index.html", visit_count=visit_count, count=message_count, messages=messages)
+
+@app.route("/new")
+def new():
+    return render_template("new.html")
+
+@app.route("/send", methods=["POST"])
+def send():
+    content = request.form["content"]
+    db.execute("INSERT INTO messages (content) VALUES (?)", [content])
+    return redirect("/")
