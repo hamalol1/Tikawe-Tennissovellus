f98d8ee8745b0b30b3190e1681a28fed22966747
Olli Hämäläinen <hamalainenolli@gmail.com>
2026-08-14 14:43:44 +0300

    Testing database

--- app.py
+++ app.py
@@ -1,20 +1,15 @@
 from flask import Flask
 from flask import render_template, request
+import sqlite3
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    words = ["apina", "banaani", "cembalo"]
-    return render_template("index.html", message="Tervetuloa!", items=words)
-
-@app.route("/order")
-def order():
-    return render_template("order.html")
-
-@app.route("/result", methods=["POST"])
-def result():
-    pizza = request.form["pizza"]
-    extras = request.form.getlist("extra")
-    message = request.form["message"]
-    return render_template("result.html", pizza=pizza, extras=extras, message=message)
+    db = sqlite3.connect("database.db")
+    db.execute("INSERT INTO visits (visited_at) VALUES (datetime('now'))")
+    db.commit()
+    result = db.execute("SELECT COUNT(*) FROM visits").fetchone()
+    count = result[0]
+    db.close()
+    return "Sivua on ladattu " + str(count) + " kertaa"
