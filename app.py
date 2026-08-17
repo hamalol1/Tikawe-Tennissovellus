802a5f45d10537eb85d887b31b215891676dd4f1
Olli Hämäläinen <hamalainenolli@gmail.com>
2026-08-17 19:21:33 +0300

    Added more functionalities to the website: - Threads - Able to edit and remove messages

--- app.py
+++ app.py
@@ -3,20 +3,15 @@ from flask import Flask, redirect, render_template, request, session
 from werkzeug.security import check_password_hash, generate_password_hash
 import config
 import db
+import forum
 
 app = Flask(__name__)
 app.secret_key = config.secret_key
 
 @app.route("/")
 def index():
-    db.execute("INSERT INTO visits (visited_at) VALUES (datetime('now'))")
-    visits_result = db.query("SELECT COUNT(*) FROM visits")
-    visit_count = visits_result[0][0]
-    
-    messages = db.query("SELECT content FROM messages")
-    message_count = len(messages)
-    
-    return render_template("index.html", visit_count=visit_count, count=message_count, messages=messages)
+    threads = forum.get_threads()
+    return render_template("index.html", threads=threads)
 
 @app.route("/register")
 def register():
@@ -44,11 +39,18 @@ def login():
     username = request.form["username"]
     password = request.form["password"]
     
-    sql = "SELECT password_hash FROM users WHERE username = ?"
-    password_hash = db.query(sql, [username])[0][0]
+    sql = "SELECT id, password_hash FROM users WHERE username = ?"
+    result = db.query(sql, [username])
+    
+    if not result:
+        return "VIRHE: väärä tunnus tai salasana"
+        
+    user_id = result[0]["id"]
+    password_hash = result[0]["password_hash"]
 
     if check_password_hash(password_hash, password):
         session["username"] = username
+        session["user_id"] = user_id
         return redirect("/")
     else:
         return "VIRHE: väärä tunnus tai salasana"
@@ -56,14 +58,53 @@ def login():
 @app.route("/logout")
 def logout():
     del session["username"]
+    del session["user_id"]
     return redirect("/")
 
-@app.route("/new")
-def new():
-    return render_template("new.html")
-
-@app.route("/send", methods=["POST"])
-def send():
+@app.route("/new_thread", methods=["POST"])
+def new_thread():
+    title = request.form["title"]
     content = request.form["content"]
-    db.execute("INSERT INTO messages (content) VALUES (?)", [content])
-    return redirect("/")
+    user_id = session["user_id"]
+
+    thread_id = forum.add_thread(title, content, user_id)
+    return redirect("/thread/" + str(thread_id))
+
+@app.route("/thread/<int:thread_id>")
+def show_thread(thread_id):
+    thread = forum.get_thread(thread_id)
+    messages = forum.get_messages(thread_id)
+    return render_template("thread.html", thread=thread, messages=messages)
+
+@app.route("/new_message", methods=["POST"])
+def new_message():
+    content = request.form["content"]
+    user_id = session["user_id"]
+    thread_id = request.form["thread_id"]
+
+    forum.add_message(content, user_id, thread_id)
+    return redirect("/thread/" + str(thread_id))
+
+@app.route("/edit/<int:message_id>", methods=["GET", "POST"])
+def edit_message(message_id):
+    message = forum.get_message(message_id)
+
+    if request.method == "GET":
+        return render_template("edit.html", message=message)
+
+    if request.method == "POST":
+        content = request.form["content"]
+        forum.update_message(message["id"], content)
+        return redirect("/thread/" + str(message["thread_id"]))
+
+@app.route("/remove/<int:message_id>", methods=["GET", "POST"])
+def remove_message(message_id):
+    message = forum.get_message(message_id)
+
+    if request.method == "GET":
+        return render_template("remove.html", message=message)
+
+    if request.method == "POST":
+        if "continue" in request.form:
+            forum.remove_message(message["id"])
+        return redirect("/thread/" + str(message["thread_id"]))
